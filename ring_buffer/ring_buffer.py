86f67b7e990901b50694fc075a412538675fdc3c
playtez <pleitez.aaron12@gmail.com>
2020-04-24 10:16:52 -0500

    ring_buffer is completed along with pip files installed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,40 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if the self.storage.length(length of nodes) < self.capacity (limit set by instance)
+        if self.storage.length < self.capacity:
+        #    # add the item to the tail of the list
+            self.storage.add_to_tail(item) 
+            self.current = self.storage.head
+        #    #  [a,b,c,d] is th tail
+        #  head is still equal to none and tail has 5 items
+        # if the length is equal to the capacity
+        elif self.storage.length == self.capacity:
+            head = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            if head == self.current:
+            # the new tail becomes the new current
+                self.current = self.storage.tail
+                
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
+        node = self.current 
+        list_buffer_contents.append(node.value)
         # TODO: Your code here
+        if node.next is None:
+            next_node = self.storage.head
+        else:
+            next_node = node.next
+        
+        while next_node != node:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node + self.storage.head
 
         return list_buffer_contents
 
